9e3fcb47ee9cb18997062dd4045bb30c4f2e7d2f
Alex <alexxcamargo1000@gmail.com>
2023-05-29 16:35:29 -0300

    fix: remove header mobile

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,14 +1,8 @@
 import { Outlet } from 'react-router-dom'
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
-import { HeaderMobile } from '../components/Header/Mobile'
-import { useAuth } from '../hooks/useAuth'
 
 export function DefaultLayout() {
-  const { user } = useAuth()
-
-  const widthScreen = window.screen.width
-
   return (
     <div>
       <Header />
